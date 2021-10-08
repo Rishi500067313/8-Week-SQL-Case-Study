55c4fff8affbaa7faa7b9d8808d891c23bcdf540
Rishi pandey <50805925+Rishi500067313@users.noreply.github.com>
2021-10-08 15:00:49 +0530

    Create week1_solutions.sql

---
+++ week1_solutions.sql
@@ -0,0 +1,7 @@
+1. SELECT s.customer_id, SUM(me.price * s.product_id) 
+     FROM dannys_diner.sales s 
+          JOIN dannys_diner.menu me ON s.product_id = me.product_id
+     GROUP BY s.customer_id;
+     
+2. SELECT customer_id, COUNT(DISTINCT(order_date)) FROM dannys_diner.sales GROUP BY customer_id;
+
