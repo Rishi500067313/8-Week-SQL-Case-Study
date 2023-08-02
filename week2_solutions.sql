7ffa8c8483de1887e4507b91894aaf549ab40591
Rishi pandey <50805925+Rishi500067313@users.noreply.github.com>
2023-08-02 17:35:44 +0530

    Update week2_solutions.sql

--- week2_solutions.sql
+++ week2_solutions.sql
@@ -2,6 +2,11 @@
 
 2. select Count(distinct customer_id) from pizza_runner.customer_orders
   
-3.  select Count(*) from pizza_runner.runner_orders where NULLIF(cancellation,'null') is NULL
+3. select Count(*) from pizza_runner.runner_orders where NULLIF(cancellation,'null') is NULL
 
-4.
+4. select c.pizza_id, count(*) from pizza_runner.customer_orders c 
+   join pizza_runner.runner_orders r on c.order_id = r.order_id 
+   where NULLIF(r.cancellation,'null') is NULL
+   group by pizza_id
+
+5. 
