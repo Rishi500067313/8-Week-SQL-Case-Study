5cf2067b8ae7a24c5d40a7e9538d5392a10db05f
Rishi pandey <50805925+Rishi500067313@users.noreply.github.com>
2023-09-25 00:28:35 +0530

    Update week3_solutions.sql

--- week3_solutions.sql
+++ week3_solutions.sql
@@ -1,1 +1,7 @@
+select count(distinct customer_id) from foodie_fi.subscriptions;
+select distinct customer_id from foodie_fi.subscriptions;
 
+select customer_id, plan_id, plan_name, start_date from foodie_fi.subscriptions join foodie_fi.plans using (plan_id) where customer_id = 1;
+select customer_id, plan_id, plan_name, start_date from foodie_fi.subscriptions join foodie_fi.plans using (plan_id) where customer_id = 23;
+select customer_id, plan_id, plan_name, start_date from foodie_fi.subscriptions join foodie_fi.plans using (plan_id) where customer_id = 333;
+select customer_id, plan_id, plan_name, start_date from foodie_fi.subscriptions join foodie_fi.plans using (plan_id) where customer_id = 533;
