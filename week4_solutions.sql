a8ce853cf4a2ec72853ab56e790e64f9d066c94f
Rishi pandey <50805925+Rishi500067313@users.noreply.github.com>
2023-10-27 14:13:14 +0530

    Update week4_solutions.sql

--- week4_solutions.sql
+++ week4_solutions.sql
@@ -4,6 +4,7 @@ select region_id, count(distinct node_id) from data_bank.customer_nodes group by
 
 select region_id, count(customer_id) from data_bank.customer_nodes group by region_id;
 
+
 with days as (
 select customer_id, node_id, end_date - start_date as num_days from data_bank.customer_nodes where end_date != '9999-12-31' group by customer_id, node_id, start_date, end_date
 ),
@@ -11,3 +12,9 @@ total_days as (
 select customer_id, node_id, sum(num_days) as node_total_days from days group by customer_id, node_id
 )
 select round(avg(node_total_days)) as average from total_days
+
+
+with days as (
+select customer_id, region_id, node_id, end_date - start_date as num_days from data_bank.customer_nodes where end_date != '9999-12-31' group by customer_id, node_id, start_date, end_date
+)
+select region_name, AVG(num_days) as node_total_days from days d JOIN data_bank.regions r on r.region_id = d.region_id group by d.region_id
